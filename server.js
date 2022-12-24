cb756e2020ac782675a8e4d108f6ec01a286809e
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2022-12-24 14:56:48 -0500

    adds HTML route

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ app.get('/', function (req, res) {
    <html>
     <body>
      <h1>Life is amazing</h1>
-    </body>
+     <h3>Let's start this morning with a diferent start before we start.</h3>
+     <ol>
+     <li>Don't look at your phone yet 'Believe me he can wait'.</li>
+     <li>Seat on Bed, streach and think how amazing life it is.</li>
+     <li></li>
+     <li></li>
+     <li></li>
+     <li></li>
+     <li></li>
+     </ol>
+     </body>
    `);
 });
 
